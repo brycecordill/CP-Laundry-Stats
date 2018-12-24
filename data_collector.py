a25647e6c2baef0456b49a3ea6e816d9b7b21182
Bryce Cordill <brycecordill@gmail.com>
2018-12-23 17:26:01 -0800

    Fixed file formatting bug

--- data_collector.py
+++ data_collector.py
@@ -57,8 +57,10 @@ class DataCollector:
                 file_data = fn.readlines()  # Read lines into a list
 
             line_num = 0
-            while file_data[line_num] != formatted_str[:7] and line_num < len(file_data) - 1:
+            while file_data[line_num][:7] != formatted_str[:7] and line_num < len(file_data) - 1:
                 line_num += 1  # Iterate through lines to find match
+                print(formatted_str[:7])
+                print(file_data[line_num])
 
             if line_num == len(file_data):  # If line doesn't yet exist
                 file_data.append(formatted_str)
